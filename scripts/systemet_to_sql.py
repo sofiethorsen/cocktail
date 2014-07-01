7a42d73ea6064590ff92dcad7d8a333d104f4f32
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2014-07-01 20:28:06 +0100

    Fix indentation

--- scripts/systemet_to_sql.py
+++ scripts/systemet_to_sql.py
@@ -6,41 +6,41 @@ from xmlconverter import convert, fromAttribute, computedBy
 from utilities import change_category
 
 def stringify(value):
-        # replace ' with '' and wrap in ''
-        return '\''+value.replace('\'', '\'\'')+'\''
+  # replace ' with '' and wrap in ''
+  return '\'' + value.replace('\'', '\'\'') + '\''
 
 def create_sql_insert(values):
-        sorted = []
+  sorted = []
 
-        for column in schema:
-                value = values[column]
+  for column in schema:
+    value = values[column]
 
-                if type(value) is str or type(value) is unicode:
-                        value = stringify(value)
+    if type(value) is str or type(value) is unicode:
+      value = stringify(value)
 
-                if not value:
-                        value = 'NULL'
+    if not value:
+      value = 'NULL'
 
-                sorted.append(unicode(value))
+    sorted.append(unicode(value))
 
-        return 'INSERT INTO articles VALUES('+','.join(sorted)+');\n'
+  return 'INSERT INTO articles VALUES('+','.join(sorted)+');\n'
 
 schema = [
-        'name',
-        'name2',
-        'category'
+  'name',
+  'name2',
+  'category'
 ]
 
 mappings = {
-        'name': fromAttribute('Namn', unicode),
-        'name2' : fromAttribute('Namn', unicode, 'Namn2'),
-        'category': fromAttribute('Varugrupp', change_category)
+  'name': fromAttribute('Namn', unicode),
+  'name2' : fromAttribute('Namn', unicode, 'Namn2'),
+  'category': fromAttribute('Varugrupp', change_category)
 }
 
 
 if __name__ == "__main__":
-        input = sys.stdin
-        if len(sys.argv) == 2:
-                input = open(sys.argv[1])
+  input = sys.stdin
+  if len(sys.argv) == 2:
+    input = open(sys.argv[1])
 
-        convert(input, 'artikel', schema, mappings, output_func=create_sql_insert)
+  convert(input, 'artikel', schema, mappings, output_func=create_sql_insert)
