683d90f884cc43494c02bcfa20ae8129c52aa355
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2014-08-07 21:37:37 +0100

    Reformat file

--- cocktail/db.py
+++ cocktail/db.py
@@ -12,89 +12,107 @@ DB_URI = 'postgresql://drinkappuser@localhost/drinkapp'
 engine = create_engine(DB_URI, encoding='utf-8')
 session = sessionmaker(bind=engine)()
 
+
 def add(model):
-	session.add(model)
+    session.add(model)
+
 
 def commit():
-	session.commit()
+    session.commit()
+
 
 def add_model(model):
-  session.add(model)
-  session.commit()
+    session.add(model)
+    session.commit()
+
 
 def all_recipes():
-  return session.query(Recipe).all()
+    return session.query(Recipe).all()
+
 
 def articles_by_exact_name(name):
-  return session.query(Article).filter(
-    (func.lower(Article.name) == func.lower(name)) |
-    (func.lower(Article.name2) == func.lower(name))).all()
+    return session.query(Article).filter(
+        (func.lower(Article.name) == func.lower(name)) |
+        (func.lower(Article.name2) == func.lower(name))).all()
+
 
 def article_by_name(string):
-  return session.query(Article).filter(Article.name.ilike('%' + string + '%')).all()
+    return session.query(Article).filter(Article.name.ilike('%' + string + '%')).all()
+
 
 def articles_by_type(type):
-  return session.query(Article).filter(
-    (func.lower(Article.type) == func.lower(type))).all()
+    return session.query(Article).filter(
+        (func.lower(Article.type) == func.lower(type))).all()
+
 
 def recipe_by_recipe_item(recipe_item):
-  return session.query(Recipe).filter_by(_id=recipe_item.recipe_id).all()
+    return session.query(Recipe).filter_by(_id=recipe_item.recipe_id).all()
+
 
 def recipe_items_by_recipe_id(recipe):
-  return session.query(RecipeItem).filter_by(recipe_id=recipe._id).all()
+    return session.query(RecipeItem).filter_by(recipe_id=recipe._id).all()
+
 
 def recipe_items_by_ingredient_name(ingredient_item):
-  if ingredient_item.name2:
-    return session.query(RecipeItem).filter(RecipeItem.name.ilike(ingredient_item.name2)).all()
-  else:
-    return session.query(RecipeItem).filter(RecipeItem.name.ilike(ingredient_item.name)).all()
+    if ingredient_item.name2:
+        return session.query(RecipeItem).filter(RecipeItem.name.ilike(ingredient_item.name2)).all()
+    else:
+        return session.query(RecipeItem).filter(RecipeItem.name.ilike(ingredient_item.name)).all()
+
 
 def recipe_item_by_recipe(recipe):
-  return session.query(RecipeItem).filter_by(recipe_id=recipe._id).all()
+    return session.query(RecipeItem).filter_by(recipe_id=recipe._id).all()
+
 
 def articles_by_name_or_type(name):
-  return session.query(Article).filter(
-    Article.name.ilike('%' + name + '%') | 
-    Article.name2.ilike('%' + name + '%') |
-    Article.type.ilike('%' + name + '%')).all()
+    return session.query(Article).filter(
+        Article.name.ilike('%' + name + '%') |
+        Article.name2.ilike('%' + name + '%') |
+        Article.type.ilike('%' + name + '%')).all()
+
 
 def recipe_items_by_ingredient_type(type):
-  return session.query(RecipeItem).filter(
-    (func.lower(RecipeItem.name) == func.lower(type))).all()
+    return session.query(RecipeItem).filter(
+        (func.lower(RecipeItem.name) == func.lower(type))).all()
+
 
 def ingredients_by_name(string):
-  return session.query(Ingredient).filter(
-    Ingredient.name.ilike('%' + string + '%') |
-    Ingredient.name2.ilike('%' + string + '%')).all()
+    return session.query(Ingredient).filter(
+        Ingredient.name.ilike('%' + string + '%') |
+        Ingredient.name2.ilike('%' + string + '%')).all()
+
 
 def ingredients_by_exact_name2(string):
-  return session.query(Ingredient).filter(Ingredient.name2.ilike(string)).all()
+    return session.query(Ingredient).filter(Ingredient.name2.ilike(string)).all()
+
 
 def ingredients_by_exact_name(string):
-  return session.query(Ingredient).filter(Ingredient.name.ilike(string)).all() 
+    return session.query(Ingredient).filter(Ingredient.name.ilike(string)).all()
+
 
 def ingredients_by_exact_names(string):
-  name2_results = ingredients_by_exact_name2(string)
-  if name2_results:
-    return name2_results
-  else:
-    return ingredients_by_exact_name(string)
+    name2_results = ingredients_by_exact_name2(string)
+    if name2_results:
+        return name2_results
+    else:
+        return ingredients_by_exact_name(string)
+
 
 def distinct_types(input_types):
-  result = set()
-  types = set()
+    result = set()
+    types = set()
 
-  for word in input_types:
-    for key, values in categories.case_insensitive_categories.items():
-      if word.lower() in values:
-        types.add(categories.all_categories[key])
+    for word in input_types:
+        for key, values in categories.case_insensitive_categories.items():
+            if word.lower() in values:
+                types.add(categories.all_categories[key])
 
-  for type in types:
-    descriptions = type.split(', ')
-    if len(descriptions) > 1:
-      for desc in descriptions: 
-        result.add(desc)
-    else:
-        result.add(type)
+    for type in types:
+        descriptions = type.split(', ')
+        if len(descriptions) > 1:
+            for desc in descriptions:
+                result.add(desc)
+        else:
+            result.add(type)
 
-  return result
+    return result
