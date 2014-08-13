d8fc367b31c398ab4ea4790bba97dc952efbcda7
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2014-08-13 19:59:04 +0100

    Fix mapping

--- cocktail/map_articles_to_ingredients.py
+++ cocktail/map_articles_to_ingredients.py
@@ -9,18 +9,11 @@ for recipe in db.all_recipes():
     ingredients = set()
     for recipe_item in db.recipe_items_by_recipe_id(recipe):
 
-        # only search by type, reducing search space and bad matching
-        if recipe_item.is_type:
-            for article in db.articles_by_type(recipe_item.name):
-                if article not in ingredients:
-                    ingredients.add(article)
-                    ingredient = Ingredient(article.name, article.name2, article.type)
-
-        else:
-            for article in db.articles_by_name_or_type(recipe_item.name):
-                if article not in ingredients:
-                    ingredients.add(article)
-                    ingredient = Ingredient(article.name, article.name2, article.type)
-                    db.add(ingredient)
+        name = recipe_item.name
+        for article in db.article_by_name_or_type(name):
+            if article.name not in ingredients:
+                ingredients.add(article.name)
+                ingredient = Ingredient(article.name, article.name2, article.type)
+                db.add(ingredient)
 
 db.commit()
