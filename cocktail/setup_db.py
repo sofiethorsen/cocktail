db9394510b143b3c9f2f86fa3c47d69b74b2f5c4
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2014-08-17 15:34:08 +0100

    Refactor setup_db

--- cocktail/setup_db.py
+++ cocktail/setup_db.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from cocktail.parser import parse_ingredients_from_file, parse_recipes_from_file
+
+from cocktail import parser
 
 from model import Ingredient, Recipe, RecipeItem
 from utilities import encode
 
 import db
 
-ingredients = parse_ingredients_from_file('../resources/ingredients.txt')
-recipes = parse_recipes_from_file('../resources/recipes.txt')
+ingredients = parser.parse_ingredients_from_file('../resources/ingredients.txt')
+recipes = parser.parse_recipes_from_file('../resources/recipes.txt')
+
+
+def create_ingredient_from_text(entry):
+    return Ingredient(entry, None, entry)
+
+
+def create_recipe_from_recipe_object(recipe_object):
+    return Recipe(recipe_object.name, recipe_object.description)
+
+
+def create_recipe_item_from_recipe(recipe_object):
+    for substance, amount, metric in recipe_object.ingredients:
+        substance = encode(substance)
+
+        if not amount:
+            return RecipeItem(recipe_object._id, substance, None)
+        else:
+            return RecipeItem(recipe_object._id, substance, encode(amount + ' ' + metric))
+
 
 # add all ingredients to db
 for ingredient in ingredients:
-    Ingredient(ingredient, None, ingredient)
-    db.add_model(Ingredient(ingredient, None, ingredient))
+    db.add_model(create_ingredient_from_text(ingredient))
 
 # add all recipes and the recipe items mapped to them
 for recipe in recipes:
-    recipe_entry = Recipe(recipe.name, recipe.description)
-    db.add_model(recipe_entry)
+    recipe_entry = create_recipe_from_recipe_object(recipe)
 
-    for substance, amount, metric in recipe.ingredients:
-        substance = encode(substance)
-        if not amount:
-            recipe_item = RecipeItem(recipe_entry._id, substance, None)
-        else:
-            tail = encode(amount + ' ' + metric)
-            recipe_item = RecipeItem(recipe_entry._id, substance, tail)
-        db.add_model(recipe_item)
+    db.add_model(recipe_entry)
+    db.add_model(create_recipe_item_from_recipe(recipe_entry))
