3d09b459a50a3d2a55df64c1c5a3635a2eed0f6b
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2014-08-17 11:35:18 +0100

    Do nullcheck

--- cocktail/cocktail.py
+++ cocktail/cocktail.py
@@ -41,28 +41,31 @@ def search_ingredient(ingredient=None):
     was_type, search_term = utilities.is_type(ingredient)
     if was_type:
         for ingredient in db.ingredient_by_type(search_term):
-            result.append(dict(
-                name=ingredient.name,
-                name2=ingredient.name2,
-                type=ingredient.type,
-                categorySearch=was_type))
+            if ingredient:
+                result.append(dict(
+                    name=ingredient.name,
+                    name2=ingredient.name2,
+                    type=ingredient.type,
+                    categorySearch=was_type))
     else:
         was_substring_of_type, types = utilities.is_substring_of_type(ingredient)
         if was_substring_of_type:
             for match in types:
                 entry = db.ingredient_by_type(match)[0]
-                result.append(dict(
-                    name=entry.name,
-                    name2=entry.name2,
-                    type=entry.type,
-                    categorySearch=was_substring_of_type
-                ))
+                if entry:
+                    result.append(dict(
+                        name=entry.name,
+                        name2=entry.name2,
+                        type=entry.type,
+                        categorySearch=was_substring_of_type
+                    ))
         for ingredient in db.ingredients_by_name(ingredient):
-            result.append(dict(
-                name=ingredient.name,
-                name2=ingredient.name2,
-                type=ingredient.type,
-                categorySearch=was_type))
+            if ingredient:
+                result.append(dict(
+                    name=ingredient.name,
+                    name2=ingredient.name2,
+                    type=ingredient.type,
+                    categorySearch=was_type))
 
     return jsonify(result=result)
 
