b8142e81b3551c519fa27007c8a229cdf8929315
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2014-08-17 11:51:40 +0100

    Make the mapping smarter by looking at type

--- cocktail/map_articles_to_ingredients.py
+++ cocktail/map_articles_to_ingredients.py
@@ -3,17 +3,25 @@
 
 import db
 
+import utilities
 from model import Ingredient
 
 for recipe in db.all_recipes():
-    ingredients = set()
+    found_articles = set()
     for recipe_item in db.recipe_items_by_recipe_id(recipe):
 
-        name = recipe_item.name
-        for article in db.article_by_name_or_type(name):
-            if article.name not in ingredients:
-                ingredients.add(article.name)
-                ingredient = Ingredient(article.name, article.name2, article.type)
-                db.add(ingredient)
+        was_type, alcohol_type = utilities.is_type(recipe_item.name.lower())
+        if was_type:
+            for article in db.articles_by_type(alcohol_type):
+                if article not in found_articles:
+                    found_articles.add(article)
+                    ingredient = Ingredient(article.name, article.name2, article.type)
+                    db.add(ingredient)
+        else:
+            for article in db.article_by_name_or_type(recipe_item.name):
+                if article not in found_articles:
+                    found_articles.add(article)
+                    ingredient = Ingredient(article.name, article.name2, article.type)
+                    db.add(ingredient)
 
 db.commit()
