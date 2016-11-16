46b6c353a39b5a3ac05702d2638bf1a5a90b64ea
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2016-11-16 15:13:37 +0000

    Allow cross origin requests for development

--- cocktail/cocktail.py
+++ cocktail/cocktail.py
@@ -4,6 +4,9 @@
 __author__ = 'thorsen'
 
 from flask import Flask, jsonify, request, send_file
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
 
 import db
 import utilities
@@ -13,12 +16,55 @@ import config
 app = Flask(__name__, static_url_path='', static_folder='../static')
 app.config.from_pyfile('config.py')
 
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 @app.route('/')
+@crossdomain(origin='*')
 def root():
     return send_file('../templates/index.html')
 
 
 @app.route('/recipe/<recipe>')
+@crossdomain(origin='*')
 def search_recipe(recipe=None):
     recipe = db.recipe_by_name(recipe)
     ingredients = _ingredients_dict_from_list(db.recipe_item_by_recipe(recipe))
@@ -41,6 +87,7 @@ def _ingredients_dict_from_list(ingredients_list):
 
 
 @app.route('/ingredients/<ingredient>')
+@crossdomain(origin='*')
 def search_ingredient(ingredient=None):
     result = []
 
@@ -120,7 +167,6 @@ def add_ingredient(recipes_dict, recipe, recipe_item):
         recipes_dict[recipe.name]['nonMatches'] = all_ingredients
     else:
         recipes_dict[recipe.name]['matches'].add(recipe_item.name)
-
 
 if __name__ == "__main__":
     app.run(port=config.PORT, host=config.HOST, debug=config.DEBUG)
